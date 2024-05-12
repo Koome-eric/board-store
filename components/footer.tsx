d374aa2bdde727a1d9150c4f3358da55b3c150a9
Koome-eric <73523878+Koome-eric@users.noreply.github.com>
2024-05-12 14:07:38 +0300

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="flexCenter mb-16 bg-custom-pink pt-11">
+    <footer className="flexCenter mb-16 bg-custom-pink pt-11 mt-10">
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" passHref>
