049042a574e2245817e09d9888d149d5d15260d4
Koome-eric <73523878+Koome-eric@users.noreply.github.com>
2024-05-12 14:55:21 +0300

    Update main-nav.tsx

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,44 +1,46 @@
 "use client";
 
-import Link from "next/link"
+import Link from "next/link";
+import { useState } from "react";
 import { usePathname } from "next/navigation";
-
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import { Category } from "@/types";
 
 interface MainNavProps {
   data: Category[];
 }
 
-const MainNav: React.FC<MainNavProps> = ({
-  data
-}) => {
+const MainNav: React.FC<MainNavProps> = ({ data }) => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false); // State to manage menu toggle
 
-  const routes = data.map((route) => ({
-    href: `/category/${route.id}`,
-    label: route.name,
-    active: pathname === `/category/${route.id}`,
+  const routes = data.map((category) => ({
+    href: `/category/${category.id}`,
+    label: category.name,
+    active: pathname === `/category/${category.id}`,
   }));
 
   return (
-    <nav
-      className="mx-6 flex items-center space-x-4 lg:space-x-6"
-    >
-      {routes.map((route) => (
-        <Link
-          key={route.href}
-          href={route.href}
-          className={cn(
-            'text-sm font-medium transition-colors hover:text-black',
-            route.active ? 'text-black' : 'text-neutral-500'
-          )}
-        >
-          {route.label}
-      </Link>
-      ))}
+    <nav className="mx-6 flex items-center md:space-x-6">
+      <button className="md:hidden mb-4" onClick={() => setIsOpen(!isOpen)}>
+        <img src="/menu-icon.svg" alt="Menu" width={25} height={25} />
+      </button>
+      <div className={`${isOpen ? 'flex' : 'hidden'} flex-col md:flex md:flex-row md:items-center md:space-x-6 w-full`}>
+        {routes.map((route) => (
+          <Link
+            key={route.href}
+            href={route.href}
+            className={cn(
+              'text-sm font-medium transition-colors hover:text-black py-2',
+              route.active ? 'text-black' : 'text-neutral-500'
+            )}
+          >
+            {route.label}
+          </Link>
+        ))}
+      </div>
     </nav>
-  )
+  );
 };
 
 export default MainNav;
