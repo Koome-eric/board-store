cbf0df22e5a05d6accf5c8a34c931ed9c4485d6f
Koome-eric <73523878+Koome-eric@users.noreply.github.com>
2024-05-12 13:56:36 +0300

    Update product-list.tsx

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -13,7 +13,7 @@ const ProductList: React.FC<ProductListProps> = ({
 }) => {
   return (
     <div>
-      <h3 className="font-bold text-3xl mb-6 text-center">{title}</h3>
+      <h3 className="font-bold text-3xl mb-6 text-center mt-16">{title}</h3>
       {items.length === 0 && <NoResults />}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
         {items.map((item) => (
