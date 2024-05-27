f51b5a2c4dafd1ba5c44f7b78af22737d33b7ae9
Koome-eric <erickomee4195@gmail.com>
2024-05-27 14:38:32 +0300

    Update Promotion component to fix useEffect dependency issue

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -7,13 +7,9 @@ import FooterComponent from './FooterComponent/footercomponent';
 const Footer = () => {
   
   return (
-<<<<<<< HEAD:components/Footer/footer.tsx
     <footer>
     <FooterComponent /> {/* Include FooterComponent above the current footer */}
     <div className="flexCenter mb-16 bg-custom-pink pt-11">
-=======
-    <footer className="flexCenter mb-16 bg-custom-pink pt-11 mt-10">
->>>>>>> bda2e65891d3d15f0c2389800f5a88f002c2ffd4:components/footer.tsx
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" passHref>
