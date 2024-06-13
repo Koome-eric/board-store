9b80385d1de6c5e45422b5131ec504f2e4cfcb32
Koome-eric <erickomee4195@gmail.com>
2024-06-13 20:09:14 +0300

    Billboard changes

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Billboard as BillboardType } from "@/types";
 
 interface BillboardProps {
@@ -5,12 +7,17 @@ interface BillboardProps {
 }
 
 const Billboard: React.FC<BillboardProps> = ({ data }) => {
-  return ( 
+  return (
     <div className="px-0 sm:px-0 lg:px-0 w-screen overflow-hidden">
       <div style={{ backgroundImage: `url(${data?.imageUrl})` }} className="relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover flex">
         <div 
           className="w-1/2 bg-custom-red bg-opacity-80 flex items-center justify-start pl-16 py-2 relative" 
-          style={{ backgroundImage: `url('/background.svg')`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover' }}
+          style={{
+            backgroundImage: `url('background.svg')`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundSize: '90%', // Default size
+          }}
         >
           <div className="font-bold text-2xl sm:text-5xl lg:text-5xl text-white relative z-10">
             {data.label}
@@ -20,6 +27,13 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
           {/* Additional content or empty space; adjust as needed */}
         </div>
       </div>
+      <style jsx>{`
+        @media (max-width: 768px) {
+          .bg-custom-red {
+            background-size: 90% !important; // Smaller size on smaller screens
+          }
+        }
+      `}</style>
     </div>
   );
 };
